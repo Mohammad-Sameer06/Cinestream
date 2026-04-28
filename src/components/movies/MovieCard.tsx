140e8b2dd82f20ea818b6dd92fdecd509ea97a5a
Sameer <itsmesameer25@gmail.com>
2026-04-29 03:14:10 +0530

    Fix MovieCard overview typing

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -27,6 +27,7 @@ interface MovieCardItem {
   name?: string;
   poster_path: string | null;
   backdrop_path: string | null;
+  overview?: string;
   release_date?: string;
   first_air_date?: string;
   vote_average?: number;
